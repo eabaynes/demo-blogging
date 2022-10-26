24efe4d11f29d5630e905715f7ccc33d35967f53
Elliot Baynes <sbaynes1994@gmail.com>
2022-10-25 21:25:25 -0500

    feat: form input

--- src/index.js
+++ src/index.js
@@ -5,3 +5,19 @@ const h1 = document.createElement("h1");
 const h2 = document.createElement("h2");
 const small = document.createElement("small");
 const p = document.createElement("p");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const values = Object.fromEntries(formData.entries());
+
+  const { title, subtitle, author, content } = values;
+
+  h1.textContent = title;
+  h2.textContent = subtitle;
+  small.textContent = author;
+  p.textContent = content;
+
+  main.append(h1, h2, small, p);
+});
